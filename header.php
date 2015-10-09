6eade37f74b14ce516762af8d40447b6d204a726
robgoodfellow <otherness-space@users.noreply.github.com>
2015-10-09 11:04:01 -0400

    Roll back...

--- header.php
+++ header.php
@@ -21,24 +21,23 @@
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'sketch' ); ?></a>
-    <!-- Begin whole header -->
-    <header id="masthead" class="flex100 flex-box site-header" role="banner">
-        <!-- Begin left box -->
-        <div class="flex-box left-flex-box site-branding">
+<!-- Begin Whole Header -->
+	<header id="masthead" class="site-header" role="banner">
+<!-- Begin Left Column -->
+		<div class="site-branding">
 			<?php if ( function_exists( 'jetpack_the_site_logo' ) ) jetpack_the_site_logo(); ?>
-			<h1 class="flex-box flex100 site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="flex-box flex100 site-description"><?php bloginfo( 'description' ); ?></h2>
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</div>
-        <!-- End left box -->
-        <!-- Begin right box -->
-		<nav id="site-navigation" class="main-navigation right-flex-box" role="navigation">
+<!-- End Left Column -->
+<!-- Begin Right Column -->
+		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle"><?php _e( 'Menu', 'sketch' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 		</nav><!-- #site-navigation -->
-        <!-- End right box -->
+<!-- End Right Column -->		
 	</header><!-- #masthead -->
-    <!-- End whole header -->
-
+<!-- End WHole Header -->
 	<div id="content" class="site-content">
 		<?php if ( get_header_image() ) : ?>
 			<?php if ( is_page_template( 'portfolio-page.php' ) && ! sketch_has_featured_posts( 1 ) || ! is_page_template( 'portfolio-page.php' ) ) : ?>
